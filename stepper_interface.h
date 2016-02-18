565b283c30664be66aeacd3a4b7c005e105f11b9
JonathanGoldnauVogt <jlgoldnauvogt@gmail.com>
2016-02-18 15:07:08 -0500

    Update stepper_interface.h

--- stepper_interface.h
+++ stepper_interface.h
@@ -1,6 +1,22 @@
-// stepper_interface.h
-// Jonathan Lukas Goldnau-Vogt
-// Used to run Arduino libraries on STM32F4xx
+/*
+stepper_interface.h
+Use this file to provide c access to c++ functions. Change the file to suit your needs
+Copyright (C) 2016  Jonathan Goldnau-Vogt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 #include "stdint.h"
 #ifdef __cplusplus
 #define EXTERNC extern "C"
