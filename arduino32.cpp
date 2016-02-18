9136047fb8385d238c3e224706c4a309b357fda9
JonathanGoldnauVogt <jlgoldnauvogt@gmail.com>
2016-02-18 14:50:29 -0500

    Added License

--- arduino32.cpp
+++ arduino32.cpp
@@ -1,12 +1,29 @@
+/*
+arduino32.cpp
+Provides missing arduino macros and functions needed by the stepper library 
+Copyright (C) 2016  Jonathan Goldnau-Vogt
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include "arduino32.h"
-
-
 
 uint32_t micros(void){
     return HAL_GetuTick();
     
 }
-
 
 void pinMode(uint8_t pin, uint8_t mode){
     TM_GPIO_Init(pinPort(pin), (uint16_t)gpioPin(pin), mode ? TM_GPIO_Mode_OUT : TM_GPIO_Mode_IN , TM_GPIO_OType_PP, TM_GPIO_PuPd_NOPULL, TM_GPIO_Speed_High);
